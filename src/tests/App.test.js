1a999c7ba0fe5a773466c7e913a45a4b78b0a7f1
Thiago <thiivieira@gmail.com>
2021-11-03 19:32:10 -0300

    Testes Feitos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,39 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from '../App';
+import responseAPI from './mocks'
 
 describe('Test Rick & Morty API', () => {
 
   beforeEach(()=>{
     //Fazer o mock do fetch aqui 
-    
+    global.fetch = async () => ({json: async () => responseAPI})
+
     render(<App/>)
   })
   
-  test('Verifica se aparece o card com titulo de "Rick Sanchez"', () => {
-
+  test('Verifica se aparece o card com titulo de "Rick Sanchez"', async () => {
+    const titulo = await screen.findByRole('heading', {name: /Rick Sanchez/i})
+    expect(titulo).toBeInTheDocument()
   })
 
   test('Verifica se existem o input de texto e o botão "Buscar"', () => {
-    
+    const buscar = screen.getByRole('button')
+    const texto = screen.getByRole('textbox')
+    expect(texto).toBeInTheDocument()
+    expect(buscar).toBeInTheDocument()
   })
 
   test('Verifica se ao buscar por "Smith" aparecem 4 cards', () => {
-    
+    const buscar = screen.getByRole('button')
+    const texto = screen.getByRole('textbox')
+    userEvent.type(texto, 'smith')
+    userEvent.click(buscar)
+
+    const cards = 4;
+    const cardsF = screen.getAllByRole('article')
+
+    expect(cardsF).toHaveLength(cards)
   })
 
 })
